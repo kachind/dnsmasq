9bf6aa0027a5f68939b9bc5ff6e4f0ab62d92d89
kachind <42634028+kachind@users.noreply.github.com>
2018-12-20 19:53:00 -0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install docker
+sudo curl -sSL https://get.docker.com/ | sh
 sudo docker run --rm --cap-add=NET_ADMIN --net=host quay.io/coreos/dnsmasq \
   -d -q \
   --dhcp-range=192.168.6.1,192.168.6.254 \
