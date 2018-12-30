a0e162a0c7940a3640017f52639d8110ab599f32
kachind <42634028+kachind@users.noreply.github.com>
2018-12-30 00:14:24 -0800

    Update go_svr.sh

--- go_svr.sh
+++ go_svr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y docker.io
+sudo wget https://github.com/kachind/dnsmasq/raw/master/intel.kpxe -O /var/lib/tftpboot 
 sudo docker run -v /var/lib/tftpboot:/var/lib/tftpboot
 --rm --cap-add=NET_ADMIN --net=host quay.io/coreos/dnsmasq \
   -d -q \
