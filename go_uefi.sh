f4e16ff474f8461d30060810f3d70310b3979fd2
kachind <42634028+kachind@users.noreply.github.com>
2018-12-29 21:07:45 -0800

    Update go_uefi.sh

--- go_uefi.sh
+++ go_uefi.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-sudo apt-get install -y docker.io
 sudo docker run --rm --cap-add=NET_ADMIN --net=host quay.io/coreos/dnsmasq \
   -d -q \
-  --dhcp-range=192.168.1.1,proxy,255.255.0.0 \
+  --dhcp-range=192.168.1.1,192.168.255.255 \
   --enable-tftp --tftp-root=/var/lib/tftpboot \
-  --dhcp-vendorclass=BIOS,PXEClient:Arch:00000 \
-  --dhcp-vendorclass=UEFI32,PXEClient:Arch:00006 \
-  --dhcp-vendorclass=UEFI,PXEClient:Arch:00007 \
-  --dhcp-vendorclass=UEFI64,PXEClient:Arch:00009 \
-  --dhcp-boot=net:BIOS,undionly.kpxe \
-  --dhcp-boot=net:UEFI32,ipxe.efi \
-  --dhcp-boot=net:UEFI,ipxe.efi \
-  --dhcp-boot=net:UEFI64,ipxe.efi \
-  --pxe-service=x86PC,http://ec2-18-219-205-208.us-east-2.compute.amazonaws.com/rancheros.ipxe \
+  --dhcp-match=set:bios,option:client-arch,0 \
+  --dhcp-boot=tag:bios,undionly.kpxe \
+  --dhcp-match=set:efi32,option:client-arch,6 \
+  --dhcp-boot=tag:efi32,ipxe.efi \
+  --dhcp-match=set:efibc,option:client-arch,7 \
+  --dhcp-boot=tag:efibc,ipxe.efi \
+  --dhcp-match=set:efi64,option:client-arch,9 \
+  --dhcp-boot=tag:efi64,ipxe.efi \
+  --dhcp-userclass=set:ipxe,iPXE \
+  --pxe-service=tag:ipxe,http://ec2-18-219-205-208.us-east-2.compute.amazonaws.com/rancheros.ipxe \
+  --address=/ec2-18-219-205-208.us-east-2.compute.amazonaws.com/18-219-205-208 \
   --log-queries \
   --log-dhcp
