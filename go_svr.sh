8d79898ebf96cda93dd373c04327c4bf2476eb72
kachind <42634028+kachind@users.noreply.github.com>
2018-12-29 23:11:00 -0800

    Update go_svr.sh

--- go_svr.sh
+++ go_svr.sh
@@ -5,7 +5,7 @@ sudo docker run --rm --cap-add=NET_ADMIN --net=host quay.io/coreos/dnsmasq \
   --dhcp-range=192.168.1.1,proxy,255.255.0.0 \
   --enable-tftp --tftp-root=/var/lib/tftpboot \
   --dhcp-userclass=set:ipxe,iPXE \
-  --pxe-service=tag:#ipxe,x86PC,"PXE chainload to iPXE",undionly.kpxe \
+  --pxe-service=tag:#ipxe,x86PC,"PXE chainload to iPXE",ipxe.efi \
   --address=/ec2-18-219-205-208.us-east-2.compute.amazonaws.com/18.219.205.208 \
   --pxe-service=tag:ipxe,x86PC,"iPXE",http://ec2-18-219-205-208.us-east-2.compute.amazonaws.com/rancheros.ipxe \
   --log-queries \
