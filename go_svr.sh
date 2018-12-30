b2ada27dac21a331a3cc4aae8dab95f4bd98e3c1
kachind <42634028+kachind@users.noreply.github.com>
2018-12-30 00:19:54 -0800

    Update go_svr.sh

--- go_svr.sh
+++ go_svr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt-get install -y docker.io
-sudo wget https://github.com/kachind/dnsmasq/raw/master/intel.kpxe -O /var/lib/tftpboot 
-sudo docker run -v /var/lib/tftpboot:/var/lib/tftpboot
+sudo wget https://github.com/kachind/dnsmasq/raw/master/intel.kpxe -O /var/lib/tftpboot/intel.kpxe
+sudo docker run -v /var/lib/tftpboot:/var/lib/tftpboot \
 --rm --cap-add=NET_ADMIN --net=host quay.io/coreos/dnsmasq \
   -d -q \
   --dhcp-range=192.168.1.1,proxy,255.255.0.0 \
   --enable-tftp --tftp-root=/var/lib/tftpboot \
   --dhcp-userclass=set:ipxe,iPXE \
-  --pxe-service=tag:#ipxe,x86PC,"PXE chainload to iPXE",ipxe.efi \
+  --pxe-service=tag:#ipxe,x86PC,"PXE chainload to iPXE",intel.kpxe \
   --address=/ec2-18-219-205-208.us-east-2.compute.amazonaws.com/18.219.205.208 \
   --pxe-service=tag:ipxe,x86PC,"iPXE",http://ec2-18-219-205-208.us-east-2.compute.amazonaws.com/rancheros.ipxe \
   --log-queries \
