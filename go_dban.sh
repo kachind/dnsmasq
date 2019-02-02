59067aa1363360257413fd6e8afadf87da036d57
kachind <42634028+kachind@users.noreply.github.com>
2019-02-02 10:55:18 -0800

    Update go_dban.sh

--- go_dban.sh
+++ go_dban.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-wget -O /tmp/dban.iso https://downloads.sourceforge.net/project/dban/dban/dban-2.3.0/dban-2.3.0_i586.iso?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdban%2Ffiles%2Fdban%2Fdban-2.3.0%2Fdban-2.3.0_i586.iso%2Fdownload%3Fuse_mirror%3Dphoenixnap&ts=1549130611
+wget -O /tmp/dban.iso https://github.com/kachind/dban/raw/master/dban-2.3.0_i586.iso
 sudo mount -o loop /tmp/dban.iso /mnt
-sudo cp /mnt/\* /var/lib/tftpboot
+sudo cp -a /mnt /var/lib/tftpboot
+sudo mv /var/lib/tftpboot/mnt/* /var/lib/tftpboot
+sudo rm -r /var/lib/tftpboot/mnt
+sudo umount /mnt
 cd /var/lib/tftpboot
 sudo wget http://mirrors.tummy.com/pub/ftp.ubuntulinux.org/ubuntu/dists/precise/main/installer-i386/current/images/netboot/ubuntu-installer/i386/pxelinux.0
 sudo mkdir /var/lib/tftpboot/pxelinux.cfg
-sudo touch /var/lib/tftpboot/pxelinux.cfg/default
-sudo echo "DEFAULT autonuke
-
-LABEL autonuke
-KERNEL dban.bzi
-APPEND nuke=\"dwipe --autonuke\" silent" > /var/lib/tftpboot/pxelinux.cfg/default
-
+wget -O /var/lib/tftpboot/pxelinux.cfg/default https://raw.githubusercontent.com/kachind/dban/master/default
 sudo chmod -R 755 /var/lib/tftpboot/
 
 sleep 30
